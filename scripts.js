ca129a57ab64b5a5e9da03ecc7a1e45edf929bd5
Devlin <devlincodes@gmail.com>
2020-12-08 19:50:22 -0800

    Added link functionality, adjusteded minimum and maximum input values.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 const container = document.querySelector('#container');
-const pixels = document.querySelectorAll('.pixel');
-const heightInput = document.querySelector('#height');
-const widthInput = document.querySelector('#width');
-
+const heightInput = document.querySelector('#height-input');
+const widthInput = document.querySelector('#width-input');
 
 function createGrid(height, width) {
     container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-    
+
     for (let h = 1; h <= height; h++) {
         for (let w = 1; w <= width; w++) {
             let gridItem = document.createElement('div');
@@ -23,6 +21,14 @@ function draw() {
     this.style.backgroundColor = 'red';
 }
 
+function link() {
+    linkButton.classList.toggle('linked');
+    heightInput.classList.toggle('linked');
+    widthInput.classList.toggle('linked');
+    const linked = document.querySelectorAll('.linked');
+    linked.forEach((link) => link.addEventListener('change', changeBoth))
+}
+
 function reset() {
     const pixels = document.querySelectorAll('.pixel');
     pixels.forEach((pixel) => pixel.remove());
@@ -30,6 +36,19 @@ function reset() {
     width = parseInt(widthInput.value);
     createGrid(height, width);
 }
+
+function changeBoth(e) {
+    height = this.value;
+    width = this.value;
+    console.log({height, width});
+    console.log(this.value);
+}
+
+const linkButton = document.querySelector('#link-button');
+linkButton.addEventListener('click', link);
+
+const linked = document.querySelectorAll('.linked');
+linked.forEach((link) => link.addEventListener('change', changeBoth))
 
 const resetButton = document.querySelector('#reset-button');
 resetButton.addEventListener('click', reset);
