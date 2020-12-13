848b134990556c10b851a6e87000b27753f9956b
Devlin <devlincodes@gmail.com>
2020-12-13 07:45:23 -0800

    Adjusted linked class color, added custom color option, added paint bucket functionality.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('#container');
-//const inputs = document.querySelector('#inputs');
 const gridDimensions = document.querySelectorAll('.grid-dimensions');
 const heightInput = document.querySelector('#height-input');
 const widthInput = document.querySelector('#width-input');
@@ -10,26 +9,20 @@ function createGrid(height, width) {
 
     for (let h = 1; h <= height; h++) {
         for (let w = 1; w <= width; w++) {
-            let gridItem = document.createElement('div');
-            gridItem.classList.add('pixel')
-            gridItem.setAttribute('id', `H${h}W${w}`);
-            gridItem.addEventListener('mouseover', draw);
-            //gridItem.addEventListener('touchmove', draw);
-            container.appendChild(gridItem);
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel')
+            pixel.setAttribute('id', `H${h}W${w}`);
+            pixel.addEventListener('mouseover', draw);
+            pixel.addEventListener('dblclick', paintBucket);
+            container.appendChild(pixel);
         }
     }
 }
 
 function draw(e) {
     if (e.type === 'mouseover') {
-        //const element = document.elementFromPoint(e.clientX, e.clientY);
-        //if (element.classList.contains('pixel')) {
-        //    element.style.backgroundColor = `${color}`;
-        //}
-        //console.log(e);
         this.style.backgroundColor = `${color}`;
-    }
-    if (e.type === 'touchmove') {
+    } else if (e.type === 'touchmove') {
         const element = document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
         if (element.classList.contains('pixel')) {
             element.style.backgroundColor = `${color}`;
@@ -37,14 +30,22 @@ function draw(e) {
     }
 }
 
+function paintBucket() {
+    const doubleClass = document.querySelector('.double');
+    const pixels = document.querySelectorAll('.pixel');
+    if (doubleClass) {
+        pixels.forEach((pixel) => pixel.style.backgroundColor = color);
+    }
+}
+
 function link() {
     linkButton.classList.toggle('linked');
     gridDimensions.forEach((dimension) => {
         dimension.classList.toggle('linked');
-        if(dimension.classList.contains('linked')) {
-            dimension.addEventListener('change', changeBoth)
+        if (dimension.classList.contains('linked')) {
+            dimension.addEventListener('input', changeBoth)
         } else {
-            dimension.removeEventListener('change', changeBoth)
+            dimension.removeEventListener('input', changeBoth)
         }
     })
 };
@@ -65,22 +66,38 @@ function changeBoth(e) {
 function changeColor() {
     color = this.dataset.color;
     colorButtons.forEach((colorButton) => colorButton.classList.remove('active'));
+    colorButtons.forEach((colorButton) => colorButton.classList.remove('double'));
     this.classList.add('active');
+    const activeClass = document.querySelector('.active');
+    activeClass.addEventListener('dblclick', toggleDouble);
+}
+
+function customColor() {
+    this.dataset.color = this.value;
+    color = this.dataset.color;
+}
+
+function toggleDouble() {
+    if (this.classList.contains('active')) {
+        this.classList.add('double');
+        const doubleClass = document.querySelector('.double');
+    }
 }
 
 container.addEventListener('touchmove', draw);
-//container.addEventListener('mouseover', draw);
 
 let color = 'black';
 const colorButtons = document.querySelectorAll('.color-button');
 colorButtons.forEach((colorButton) => colorButton.addEventListener('click', changeColor));
 colorButtons.forEach((colorButton) => colorButton.style.backgroundColor = colorButton.dataset.color);
+const customButton = document.querySelector('#custom-button');
+customButton.addEventListener('input', customColor);
 
 const linkButton = document.querySelector('#link-button');
 linkButton.addEventListener('click', link);
 
 const linked = document.querySelectorAll('.linked');
-linked.forEach((link) => link.addEventListener('change', changeBoth))
+linked.forEach((link) => link.addEventListener('input', changeBoth))
 
 const resetButton = document.querySelector('#reset-button');
 resetButton.addEventListener('click', reset);
@@ -88,3 +105,7 @@ resetButton.addEventListener('click', reset);
 let height = heightInput.value;
 let width = widthInput.value;
 createGrid(height, width);
+
+
+const openClass = document.querySelectorAll('.open');
+//colorButtons.forEach((activeElement) => activeElement.addEventListener('transitionend', toggleOpen));
